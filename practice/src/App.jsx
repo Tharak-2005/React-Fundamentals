2aef52f3669d14d86ffd1068504877eea768f242
Tharak-2005 <tharakbf@gmail.com>
2025-09-13 17:08:03 +0530

    Main React

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -3,43 +3,53 @@ function App() {
   const [count,setCount]=useState(0);
   const [email,setEmail]=useState("")
   const[password,setPassword]=useState("")
+
+
+  const[formData,setFormData]=useState({
+    email:"",
+    password:""
+  })
   
-  const handleEmail=(event)=>{
-    setEmail(event.target.value)
-  }
-  const handlePassword=(event)=>{
-    setPassword(event.target.value)
-  }
-  const handleCLick=()=>{
-    alert(`email: ${email} password: ${password} Successfully login`)
-  }
-  const handleIncrement=()=>{
-    setCount((preCount)=>preCount+1)
-    console.log(count)
-  }
-  const handleDecrement=()=>{
-    setCount((preCount)=>preCount-1)
-    console.log(count)
-  }
+  // const handleEmail=(event)=>{
+  //   setEmail(event.target.value)
+  // }
+  // const handlePassword=(event)=>{
+  //   setPassword(event.target.value)
+  // }
+  // const handleCLick=()=>{
+  //   alert(`email: ${email} password: ${password} Successfully login`)
+  // }
+  // const handleIncrement=()=>{
+  //   setCount((preCount)=>preCount+1)
+  //   console.log(count)
+  // }
+  // const handleDecrement=()=>{
+  //   setCount((preCount)=>preCount-1)
+  //   console.log(count)
+  // }
   const handleChange=(event)=>{
-    console.log(event.target.name)
-    console.log(event.target.value)
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    setFormData({
+      ...formData,
+      [event.target.name]:event.target.value
+    })
   }
   const handleSubmit=(event)=>{
     event.preventDefault()
-    alert(`email: ${email} password: ${password} Successfully login`)
+    alert(`email: ${formData.email} password: ${formData.password} Successfully login`)
   }
   return (
     <div style={{textAlign:"center"}}>
       <form onSubmit={handleSubmit}>
       <p>i am a button</p>
-      <input type="text" name="email" placeholder="Type your email" onChange={handleEmail} />
-      <input type="password" name="password"placeholder="Enter the password" onChange={handlePassword} />
+      <input type="text" name="email" placeholder="Type your email" onChange={handleChange} />
+      <input type="password" name="password"placeholder="Enter the password" onChange={handleChange} />
       <button type="submit">Login</button>
       </form>
-      <p>{count}</p>
+      {/* <p>{count}</p>
       <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleDecrement}>Decrement</button> */}
 
 
     </div>
